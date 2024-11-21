53c3593e24a7938bc70f6cf78695fc228792b89a
Fluffycoal <kiptoo.brian@strathmore.edu>
2024-11-21 18:51:41 +0300

    add SQL triggers for customer info update and purchase data validation

--- 3_normal_triggers/script.sql
+++ 3_normal_triggers/script.sql
@@ -1,1 +1,25 @@
 -- Write your SQL code here
+DELIMITER //
+CREATE TRIGGER UpdateCustomerInfo
+BEFORE UPDATE ON Customer
+FOR EACH ROW
+BEGIN
+    IF @is_authorized_user IS NULL OR @is_authorized_user = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'You are not authorized to update customer information';
+    END IF;
+
+END; //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER ValidatePurchaseDataBeforeInsert
+BEFORE INSERT ON Purchase
+FOR EACH ROW
+BEGIN
+    IF NEW.Purchase_Date > CURDATE() THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Purchase date cannot be in the future';
+    END IF;
+END; //
+DELIMITER ;
