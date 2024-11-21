36ee01fe8d8d2fbe44398e72c05b37531758ce0f
Fluffycoal <kiptoo.brian@strathmore.edu>
2024-11-21 19:15:49 +0300

    add SQL indexes for improved query performance on Network_Device, Server, Customer, and Purchase tables

--- 7_indexes/script.sql
+++ 7_indexes/script.sql
@@ -1,1 +1,5 @@
 -- Write your SQL code here
+CREATE INDEX idx_Device_Location ON Network_Device (Location);
+CREATE INDEX idx_Server_type ON Server (Server_Type);
+CREATE INDEX idx_Customer_Name ON Customer (Customer_Name);
+CREATE INDEX idx_Purchase_Date ON Purchase (Purchase_Date);
